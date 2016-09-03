c02ebe699b892901aa0953325aad4b4753bc92d3
Nathan Witt <wittnl@gmail.com>
2016-09-03 00:36:53 -0500

    bonjour-http MVP

---
+++ index.js
@@ -0,0 +1,39 @@
+var srv = require('http-server');
+var bon = require('bonjour')();
+var argv = require('minimist')(process.argv.slice(2));
+
+var options = Object.assign(argv, {
+  root: argv._[0],
+  a: argv.a || '0.0.0.0',
+  p: argv.p || 8080
+});
+
+var http = srv.createServer(options);
+
+function cleanup() {
+  bon.unpublishAll(function() {
+    console.info('bonjour closed');
+    bon.destroy();
+  });
+  console.info('server closed');
+  process.exit();
+}
+
+http.listen(options.p, options.a, function() {
+
+  process.on('SIGINT', function() {
+    cleanup();
+  });
+  process.on('SIGTERM', function() {
+    cleanup();
+  });
+
+  var svc = bon.publish({
+    name: 'bonjour-http-server',
+    type: 'http',
+    port: options.p
+  });
+
+  svc.start();
+
+});
